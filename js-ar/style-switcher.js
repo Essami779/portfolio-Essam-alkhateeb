8015e413e44e16ff186e944967de3fe46a9c6d86
Essam Alkhateeb <essamalkhateeb88@gmail.com>
2025-07-05 20:32:53 +0300

    new update

--- js-ar/style-switcher.js
+++ js-ar/style-switcher.js
@@ -4,12 +4,12 @@ styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 });
 
-// hide style switcher on scroll
-// window.addEventListener("scroll", () => {
-//     if(document.querySelector(".style-switcher").classList.contains("open")) {
-//         document.querySelector(".style-switcher").classList.remove("open");
-//     }
-// })
+hide style switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
 
 /* ======================================== theme colors ================================================== */
 const alternateStyles = document.querySelectorAll(".alternate-style");
