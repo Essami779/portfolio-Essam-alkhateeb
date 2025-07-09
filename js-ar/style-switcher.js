c60a6181beba265984c69e79a30767f8d6a5c8bf
Essam Alkhateeb <essamalkhateeb88@gmail.com>
2025-07-09 08:11:05 +0300

    new fix

--- js-ar/style-switcher.js
+++ js-ar/style-switcher.js
@@ -4,7 +4,7 @@ styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 });
 
-hide style switcher on scroll
+// hide style switcher on scroll
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
